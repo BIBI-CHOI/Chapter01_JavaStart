50f438a9fab543671ee4b3d06bb759a64ab85c9a
User <User@DESKTOP-20GJ8HB>
2021-07-30 00:25:57 +0900

    2

--- src/ch01_3_java_process/Hello.java
+++ src/ch01_3_java_process/Hello.java
@@ -1,5 +1,8 @@
 package ch01_3_java_process;
 
 public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
 
 }
--- src/ch01_3_java_process/RunStatementExample.java
+++ src/ch01_3_java_process/RunStatementExample.java
@@ -1,5 +1,11 @@
 package ch01_3_java_process;
 
 public class RunStatementExample {
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		int result = x + y;
+		System.out.println(result);
+	}
 
 }
